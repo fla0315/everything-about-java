de9312197f0b57eb16bb9c8b934b8464b3f68557
rimi <fla0315@naver.com>
2024-02-18 00:10:33 +0900

    [scartchpad-java] computeIfAbsent 메서드 정리 필요

--- scratchpad-java/src/main/java/com/core/ScratchpadJavaApplication.java
+++ scratchpad-java/src/main/java/com/core/ScratchpadJavaApplication.java
@@ -1,4 +1,4 @@
-package com.scratchpadjava;
+package com.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ scratchpad-java/src/main/java/com/core/java/domain/HelloJava.java
@@ -0,0 +1,18 @@
+package com.core.java.domain;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class HelloJava {
+
+    private String code;
+    private Long obsSeq;
+    private Long obsDtSeq;
+
+}
