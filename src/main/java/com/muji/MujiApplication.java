6b778c86999817d9e546cf48fe27802b2062f5c4
denver <fla0315@naver.com>
2024-12-02 23:19:40 +0900

    feat : 프로젝트 재설정

---
+++ src/main/java/com/muji/MujiApplication.java
@@ -0,0 +1,13 @@
+package com.muji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MujiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muji/MujiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
