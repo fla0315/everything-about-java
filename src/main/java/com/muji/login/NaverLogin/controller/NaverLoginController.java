06705ed2d9dd34a8de8c65c2aac613aea7439b90
denver <fla0315@naver.com>
2024-12-02 23:50:25 +0900

    feat : 네이버 로그인 초기세팅 중

--- src/main/java/com/muji/login/NaverLogin/controller/NaverLoginController.java
+++ src/main/java/com/muji/login/NaverLogin/controller/NaverLoginController.java
@@ -1,4 +1,4 @@
-package com.muji.NaverLogin.controller;
+package com.muji.login.NaverLogin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +10,6 @@ public class NaverLoginController {
 
   @GetMapping("/login")
   public String naverLogin() {
-    System.out.println("true = " + true);
-    System.out.println("로그인!");
     return null;
   }
 
